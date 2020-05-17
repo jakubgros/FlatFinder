c1865804d8dbb12fb6dc6e20c5b1da6e5aabc845
Jakub Gros <kubagros@gmail.com>
2020-05-17 19:12:16 +0200

    [bug] TextSearcher applies comparator correctly if comparator matches operands with different length

--- src/TextSearcher.py
+++ src/TextSearcher.py
@@ -8,11 +8,12 @@ class TextSearcher:
                  exception_rules=None,
                  title_case_sensitive=False,
                  equality_comparator=lambda lhs, rhs: lhs == rhs):
-        frame_size = min(len(phrase.split()), len(text.split()))
 
-        text_frame = TextFrame(text, frame_size)
-        for slice_position, frame in text_frame:
-            if equality_comparator(phrase, frame):
-                return True, (slice_position, text_frame.all_words)
+        max_frame_size = len(phrase.split())
+        for frame_size in range(1, max_frame_size+1):
+            text_frame = TextFrame(text, frame_size)
+            for slice_position, frame in text_frame:
+                if equality_comparator(phrase, frame):
+                    return True, (slice_position, text_frame.all_words)
 
         return False, (None, text_frame.all_words)
