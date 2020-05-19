164f4516a455827f03967e8b54e36871b741a012
Jakub Gros <kubagros@gmail.com>
2020-05-19 20:27:31 +0200

    [optimisation] optimized iterating over text in search algorithm

--- src/TextSearcher.py
+++ src/TextSearcher.py
@@ -1,3 +1,5 @@
+import re
+
 from text_frame import TextFrame
 
 
@@ -9,9 +11,12 @@ class TextSearcher:
                  title_case_sensitive=False,
                  equality_comparator=lambda lhs, rhs: lhs == rhs):
 
+        word_list = re.split('(\+|\(|\)| |-|:|;|!|,|\.|\n)', text)
+        word_list = [word.strip() for word in word_list if word and word.strip()]
+
         max_frame_size = len(phrase.split())
         for frame_size in range(1, max_frame_size+1):
-            text_frame = TextFrame(text, frame_size)
+            text_frame = TextFrame(word_list, frame_size)
             for slice_position, frame in text_frame:
                 if equality_comparator(phrase, frame):
                     return True, (slice_position, text_frame.all_words)
--- src/text_frame.py
+++ src/text_frame.py
@@ -1,18 +1,15 @@
 import re
 class TextFrame:
-    def __init__(self, text, frame_size):
-        self.text = text
+    def __init__(self, word_list, frame_size):
+        self.word_list = word_list
         self.frame_size = frame_size
-
-        text_split = re.split('(\+|\(|\)| |-|:|;|!|,|\.|\n)', self.text)
-        self.text_split = [word.strip() for word in text_split if word and word.strip()]
 
     @property
     def all_words(self):
-        return self.text_split
+        return self.word_list
 
     def __iter__(self):
-        words_count = len(self.text_split)
+        words_count = len(self.word_list)
 
         if self.frame_size > words_count:
             return
@@ -20,4 +17,4 @@ class TextFrame:
         for i in range(words_count - self.frame_size + 1):
             start = i
             end = start + self.frame_size
-            yield ((start, end), " ".join(self.text_split[start:end]))
+            yield ((start, end), " ".join(self.word_list[start:end]))
