6f9b78a0f195ac79e0608db0387bc5aa9725783d
Jakub Gros <kubagros@gmail.com>
2020-05-23 21:23:15 +0200

    [refactor] cleaned up flat.py file

--- src/containers/flat.py
+++ src/containers/flat.py
@@ -1,15 +1,15 @@
+import logging
 from collections import namedtuple
-
 from selenium.common.exceptions import NoSuchElementException
-
 from other.driver import driver
 
 ExtractionResult = namedtuple('ExtractionResult', ['status', 'value'])
 
+
 class Flat:
     def __init__(self):
         self.attributes = {}
-        self.description_attributes = {}
+        self.description_extracted_attributes = {}
         self.title = None
         self.url = None
         self.price = None
@@ -23,15 +23,15 @@ class Flat:
 
     @classmethod
     def from_url(cls, url):
-        obj = cls()
-        obj.url = url
+        flat = cls()
+        flat.url = url
 
         driver.get(url)
 
-        obj.title = driver.find_element_by_class_name('myAdTitle').text
-        obj.price = cls._parse_price(driver.find_element_by_class_name('price').text)
-        obj.description = driver.find_element_by_class_name('description').text
-        obj.address = driver.find_element_by_class_name('full-address').text
+        flat.title = driver.find_element_by_class_name('myAdTitle').text
+        flat.price = cls._parse_price(driver.find_element_by_class_name('price').text)
+        flat.description = driver.find_element_by_class_name('description').text
+        flat.address = driver.find_element_by_class_name('full-address').text
 
         # top menu
         sel_menu = driver.find_element_by_class_name("selMenu")
@@ -42,13 +42,10 @@ class Flat:
                 value_elem = el.find_element_by_class_name('value')
             except NoSuchElementException:
                 continue
-            obj.attributes[name_elem.text] = value_elem.text
+            flat.attributes[name_elem.text] = value_elem.text
 
-        return obj
+        return flat
 
     def extract_info_from_description(self, extractor):
         status, attribute, value = extractor(self.description)
-        self.description_attributes[attribute] = ExtractionResult(status, value)
-        print(self.description_attributes[attribute])
-        print(self.url)
-        print(self.description)
+        self.description_extracted_attributes[attribute] = ExtractionResult(status, value)
