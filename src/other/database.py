33c6a01a385805bd7862d48378d39bf4c8a1f432
Jakub Gros <kubagros@gmail.com>
2020-07-18 21:26:21 +0200

    [refactor] refactor in database

---
+++ src/other/database.py
@@ -0,0 +1,80 @@
+from pprint import pprint
+import xml.etree.cElementTree as ET
+from mailer import Mailer
+from mailer import Message
+
+
+class Database:
+    def __init__(self):
+        self.mailer = Mailer('smtp.gmail.com', 587, use_tls=True)
+        self.mailer.login(usr="123szukaczmieszkan123@gmail.com", pwd="#Rguih1m37x")
+        self.amount_of_all_processed = 0
+        self.currently_printed_id = 0
+        self.buffer = []
+        self.buffer_size = 10
+
+    def save(self, flats):
+        for flat in flats:
+            self.buffer.append(flat)
+
+            if len(self.buffer) >= self.buffer_size:
+                self.flush()
+
+    def flush(self):
+        self._save_to_email(self.buffer)
+        self._save_to_console(self.buffer)
+
+        self.buffer.clear()
+
+    @staticmethod
+    def _save_to_console(flats):
+        for flat in flats:
+            pprint(flat.to_dict(), indent=2)
+
+    def _save_to_email(self, flats):
+        root = ET.Element("html")
+
+        previous_id = self.currently_printed_id
+        self.currently_printed_id += len(flats)
+        for flat in flats:
+            flat_dict = flat.to_dict()
+            self._display_to_html(flat_dict, root)
+
+        message = Message(From="123szukaczmieszkan123@gmail.com", To="kubagros@gmail.com")
+        message.Subject = f"MIESZKANIA [{previous_id}-{self.currently_printed_id}/{self.amount_of_all_processed}]"
+        message.Html = ET.tostring(root, method='html')
+        self.mailer.send(message)
+
+    @staticmethod
+    def _display_to_html(flat_dict, root):
+        address_attr = flat_dict['attributes'].get("Lokalizacja", "")
+        gmaps_attr = flat_dict['address']
+        extracted_addr = [addr for addr in flat_dict['description_extracted_attributes']['address'].values()]
+
+        table = ET.SubElement(root, "table", style="border: 1px solid black; background-color: #E9967A")
+
+        for label, value in [
+            ('title', flat_dict['title']),
+            ('price', flat_dict['price']),
+            ('address_attr', address_attr),
+            ('gmaps_attr', gmaps_attr),
+            ('extracted_addr', extracted_addr),
+            ('gmaps_attr', gmaps_attr),
+            ('description', flat_dict['description']),
+            ('link', flat_dict['url']),
+        ]:
+            label = str(label)
+            value = str(value)
+            tr = ET.SubElement(table, "tr")
+            ET.SubElement(tr, "td").text = label
+            ET.SubElement(tr, "td").text = value
+
+        for photo in flat_dict['photos']:
+            tr = ET.SubElement(root, "tr")
+            ET.SubElement(tr, "img", src=photo)
+
+        # SEPARATORS
+        ET.SubElement(root, "hr")
+        ET.SubElement(root, "br")
+        ET.SubElement(root, "br")
+        ET.SubElement(root, "hr")
--- src/scrapping_manager.py
+++ src/scrapping_manager.py
@@ -1,17 +1,14 @@
 import logging
 from datetime import timedelta
-from pprint import pprint
-import xml.etree.cElementTree as ET
-from mailer import Mailer
-from mailer import Message
+
 from filters.attribute_filter import AttributeFilter
 from containers.flat import Flat
 from data_provider.address_provider import address_provider
 from data_provider.gumtree_flat_provider import GumtreeFlatProvider
 
-
 from filters.exclude_address_filter import ExcludeAddressFilter
 from other.LoopTicker import LoopTicker
+from other.database import Database
 from parsers.address_extractor import AddressExtractor
 from parsers.bachelor_pad_extractor import BachelorPadExtractor
 from parsers.interconnecting_room_extractor import InterconnectingRoomExtractor
@@ -19,77 +16,6 @@ from parsers.kitchenette_extractor import KitchenetteExtractor
 from text.analysis.context_analysers.first_word_of_sentence_context import FirstWordOfSentenceContext
 from text.analysis.context_analysers.nearby_location_context import NearbyLocationContext
 from text.analysis.context_analysers.price_context import PriceContext
-
-class OutputManager():
-    def __init__(self):
-        self.mailer = Mailer('smtp.gmail.com', 587, use_tls=True)
-        self.mailer.login(usr="123szukaczmieszkan123@gmail.com", pwd="#Rguih1m37x")
-        self.amount_of_all_processed = 0
-        self.currently_printed_id = 0
-        self.buffer = []
-        self.buffer_size = 10
-
-    def output(self, flats):
-        for flat in flats:
-            self.buffer.append(flat)
-
-            if len(self.buffer) >= self.buffer_size:
-                self._output_to_email(self.buffer)
-                self._output_to_console(self.buffer)
-
-                self.buffer.clear()
-
-    def _output_to_console(self, flats):
-        for flat in flats:
-            pprint(flat.to_dict(), indent=2)
-
-    def _output_to_email(self, flats):
-        root = ET.Element("html")
-
-        previous_id = self.currently_printed_id
-        self.currently_printed_id += len(flats)
-        for flat in flats:
-            flat_dict = flat.to_dict()
-            self._display_to_html(flat_dict, root)
-
-        message = Message(From="123szukaczmieszkan123@gmail.com", To="kubagros@gmail.com")
-        message.Subject = f"MIESZKANIA [{previous_id}-{self.currently_printed_id}/{self.amount_of_all_processed}]"
-        message.Html = ET.tostring(root, method='html')
-        self.mailer.send(message)
-
-    def _display_to_html(self, flat_dict, root):
-        address_attr = flat_dict['attributes'].get("Lokalizacja", "")
-        gmaps_attr = flat_dict['address']
-        extracted_addr = [addr for addr in flat_dict['description_extracted_attributes']['address'].values()]
-
-        table = ET.SubElement(root, "table", style="border: 1px solid black; background-color: #E9967A")
-
-        for label, value in [
-            ('title', flat_dict['title']),
-            ('price', flat_dict['price']),
-            ('address_attr', address_attr),
-            ('gmaps_attr', gmaps_attr),
-            ('extracted_addr', extracted_addr),
-            ('gmaps_attr', gmaps_attr),
-            ('description', flat_dict['description']),
-            ('link', flat_dict['url']),
-        ]:
-            label = str(label)
-            value = str(value)
-            tr = ET.SubElement(table, "tr")
-            ET.SubElement(tr, "td").text = label
-            ET.SubElement(tr, "td").text = value
-
-        for photo in flat_dict['photos']:
-            tr = ET.SubElement(root, "tr")
-            ET.SubElement(tr, "img", src=photo)
-
-        # SEPARATORS
-        ET.SubElement(root, "hr")
-        ET.SubElement(root, "br")
-        ET.SubElement(root, "br")
-        ET.SubElement(root, "hr")
-
 
 
 class ScrappingManager:
@@ -100,7 +26,7 @@ class ScrappingManager:
 
         self.processed_flats_by_titles = {}
         self.first_run_time_delta = first_run_time_delta
-        self.output_manager = OutputManager()
+        self.output_manager = Database()
         self.loop_ticker = LoopTicker(check_interval_in_seconds)
 
     def apply_filters(self, flats):
@@ -124,11 +50,10 @@ class ScrappingManager:
                         flat.extract_info_from_description(self.extractors)
 
                         new_flats = self.apply_filters([flat])
-                        self.output_manager.output(new_flats)
+                        self.output_manager.save(new_flats)
 
                 except Exception as e:
                     logging.error(e, exc_info=True)
-
 
 
 if __name__ == "__main__":
